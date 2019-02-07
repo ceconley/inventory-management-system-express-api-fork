4fb099f294faa1577602aa1c0cc47db11ea5b46d
ceconley <ceconley4@gmail.com>
2019-02-06 22:09:26 -0500

    changed name of repo and ran npm install

--- app/routes/config/db.js
+++ app/routes/config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'inventory-management-system-express-api'
+const mongooseBaseName = 'inventory-management-system-express-api-fork'
 
 // create the mongodb uri for development and test
 const database = {
